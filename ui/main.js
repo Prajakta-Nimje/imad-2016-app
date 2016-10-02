825892140576e97c44cdd3a6ffd9c77bf8b48d45
Prajakta Nimje <prajunimje@gmail.com>
2016-10-02 04:45:37 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,15 @@ element.innerHtml = 'New Value';
 
 //Move the image
 var img=document.getElementById('madi');
+
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft+'px';
+}
+
 img.oncLick = function() {
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = '100px';
 };
